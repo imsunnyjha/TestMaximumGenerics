46066b4285c32b36856ab4620d13be402076d052
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-20 20:38:40 +0530

    [SUNNY]Add.UseCase1 for finding maximum integer among 3 numbers with required test case

--- TestMaximumWithGenerics/Program.cs
+++ TestMaximumWithGenerics/Program.cs
@@ -7,6 +7,16 @@ namespace TestMaximumWithGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Maximum Test Using Generics!");
+            MaximumValue maximumNumber = new MaximumValue();
+            Console.WriteLine("\nMaximum Integer>>>>>>>>>");
+            Console.WriteLine("\nFirst Value: ");
+            int firstIntValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nSecond Value: ");
+            int secondIntValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThird Value: ");
+            int thirdIntValue = Convert.ToInt32(Console.ReadLine());
+            int maxIntNumer = MaximumValue.GetMaximumIntegerNumber(firstIntValue, secondIntValue, thirdIntValue);
+            Console.WriteLine("Maximum :" + maxIntNumer);
         }
     }
 }
