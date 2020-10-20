4baf893fd01c2dea64f7f0f702290ef79eaed85a
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-20 20:48:58 +0530

    [SUNNY]Add.UseCase2 for finding maximum double among given 3 doubles along with test cases

--- MaximumValueTest/MaxValueTest.cs
+++ MaximumValueTest/MaxValueTest.cs
@@ -33,5 +33,32 @@ namespace MaximumValueTest
 
             Assert.AreEqual(45, max);
         }
+        [TestMethod]
+        public void GivenDouble_MaxFirstNumber_ShouldReturnFirstNumber()
+        {
+            MaximumValue maximumNumber = new MaximumValue();
+
+            double max = maximumNumber.GetMaximumDoubleNumber(30.5, 12.9, 18.6);
+
+            Assert.AreEqual(30.5, max);
+        }
+        [TestMethod]
+        public void GivenDouble_MaxSecondNumber_ShouldReturnSecondNumber()
+        {
+            MaximumValue maximumNumber = new MaximumValue();
+
+            double max = maximumNumber.GetMaximumDoubleNumber(16.5, 50.8, 24.9);
+
+            Assert.AreEqual(50.8, max);
+        }
+        [TestMethod]
+        public void GivenDouble_MaxThirdNumber_ShouldReturnThirdNumber()
+        {
+            MaximumValue maximumNumber = new MaximumValue();
+
+            double max = maximumNumber.GetMaximumDoubleNumber(15.9, 20.2, 45.5);
+
+            Assert.AreEqual(45.5, max);
+        }
     }
 }
--- TestMaximumWithGenerics/Program.cs
+++ TestMaximumWithGenerics/Program.cs
@@ -8,6 +8,10 @@ namespace TestMaximumWithGenerics
         {
             Console.WriteLine("Welcome To Maximum Test Using Generics!");
             MaximumValue maximumNumber = new MaximumValue();
+
+            ///<summary>
+            ///Integer Calling
+            ///</summary>
             Console.WriteLine("\nMaximum Integer>>>>>>>>>");
             Console.WriteLine("\nFirst Value: ");
             int firstIntValue = Convert.ToInt32(Console.ReadLine());
@@ -15,8 +19,21 @@ namespace TestMaximumWithGenerics
             int secondIntValue = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\nThird Value: ");
             int thirdIntValue = Convert.ToInt32(Console.ReadLine());
-            int maxIntNumer = MaximumValue.GetMaximumIntegerNumber(firstIntValue, secondIntValue, thirdIntValue);
-            Console.WriteLine("Maximum :" + maxIntNumer);
+            int maxIntNumber = MaximumValue.GetMaximumIntegerNumber(firstIntValue, secondIntValue, thirdIntValue);
+            Console.WriteLine("Maximum :" + maxIntNumber);
+
+            ///<summary>
+            ///Double Calling
+            ///</summary>
+            Console.WriteLine("\nMaximum Double>>>>>>>>>");
+            Console.WriteLine("\nFirst Value: ");
+            double firstDoubleValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nSecond Value: ");
+            double secondDoubleValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nThird Value: ");
+            double thirdDoubleValue = Convert.ToDouble(Console.ReadLine());
+            double maxDoubleNumber = maximumNumber.GetMaximumDoubleNumber(firstDoubleValue, secondDoubleValue, thirdDoubleValue);
+            Console.WriteLine("Maximum :" + maxDoubleNumber);
         }
     }
 }
