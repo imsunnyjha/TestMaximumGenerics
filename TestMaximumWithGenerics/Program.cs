5fa449f07bec96cb7d6d3b2792ee3fbb46b2a753
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-20 18:17:48 +0530

    [SUNNY]Add. Welcome to maximum test analyzer problem

---
+++ TestMaximumWithGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMaximumWithGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Maximum Test Using Generics!");
+        }
+    }
+}
